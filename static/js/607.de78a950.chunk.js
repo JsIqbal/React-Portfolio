d4b0a2d60a306ecca6ef1329064097d859c4d81f
JsIqbal <javascriptiqbal@gmail.com>
2022-10-23 03:31:51 +0600

    Initialize Build Version 1

---
+++ static/js/607.de78a950.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknew_app=self.webpackChunknew_app||[]).push([[607],{607:function(e,p,n){n.r(p),p.default={}}}]);
+//# sourceMappingURL=607.de78a950.chunk.js.map
---
+++ static/js/632.418e01b6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknew_app=self.webpackChunknew_app||[]).push([[632],{632:function(e,p,n){n.r(p),p.default={}}}]);
+//# sourceMappingURL=632.418e01b6.chunk.js.map
